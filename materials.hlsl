480bc9ceadd68fb965b670270bfa7210cab1c899
mlhernandez08 <08mlhernandez@gmail.com>
2026-05-08 16:36:36 -0700

    HLSL functions and displacement

--- materials.hlsl
+++ materials.hlsl
@@ -148,3 +148,95 @@ for(int i = 0; i < 256; i++)
  }
 opacityMask = 0;
 return float3(0,0,0);
+
+// Raymarching with Displacement
+
+float3 rayOrigin = 1 - (viewDir - worldPos);
+float3 rayStep = viewDir * -1;
+
+float3 lightDirection = normalize(lightPos);
+
+for(int i = 0; i < 256; i++)
+{
+    float displace = sphereCenter + (sin(rayOrigin.x * sin(time)/3) + 
+                                     sin(rayOrigin.y * sin(time)/3) +
+                                     sin(rayOrigin.z * sin(time)/3));
+
+    float dist = length(rayOrigin - displace) - sphereRadius;
+
+    if(dist < 0.01)
+    {
+        float3 normal = normalize(rayOrigin - displace);
+        float diffuse = max(dot(normal, lightDirection), 0);
+        float3 reflection = reflect(lightDirection, normal);
+        float3 viewDirection = normalize(-worldPos - rayOrigin);
+        float specular = pow(max(dot(reflection, viewDirection), 0), 200);
+
+        return (diffuse * float3(1,0,0)) + (specular * float3(1, 1, 1));
+    }
+
+    opacityMask = 1;
+    rayOrigin += rayStep;
+ }
+opacityMask = 0;
+return float3(0,0,0);
+
+// Functions - Need to be placed inside a structure
+
+struct colorOperations
+{
+    float3 colorChoose(int R, int G, int B) {
+        return float3(R, G, B);
+    }
+};
+colorOperations co;
+
+return (co.colorChoose(1, 0, 0));
+
+// Copy of RayMarched material with shape function
+
+float3 rayOrigin = 1 - (viewDir - worldPos);
+float3 rayStep = viewDir * -1;
+
+float3 lightDirection = normalize(lightPos);
+
+struct sdfShapes
+{
+    float donut(float3 p, float size, float cutout){
+        float2 q = float2(length(p.xz) - size, p.y);
+        return length(q) - cutout;
+    }
+};
+sdfShapes sdf;
+
+for(int i = 0; i < 256; i++)
+{
+
+
+    float dist = sdf.donut(rayOrigin, 50, 25);
+
+    if(dist < 0.01)
+    {
+        float eps = 0.001;
+
+        float3 normal = normalize(float3(
+            sdf.donut(float3(rayOrigin.x + eps, rayOrigin.y, rayOrigin.z), 50, 25)
+                            - sdf.donut(float3(rayOrigin.x - eps, rayOrigin.y, rayOrigin.z), 50, 25),
+            sdf.donut(float3(rayOrigin.x, rayOrigin.y + eps, rayOrigin.z), 50, 25)
+                            - sdf.donut(float3(rayOrigin.x, rayOrigin.y - eps, rayOrigin.z), 50, 25),
+            sdf.donut(float3(rayOrigin.x, rayOrigin.y, rayOrigin.z + eps), 50, 25)
+                            - sdf.donut(float3(rayOrigin.x, rayOrigin.y, rayOrigin.z - eps), 50, 25)
+        ));
+        float diffuse = max(dot(normal, lightDirection), 0);
+        float3 reflection = reflect(lightDirection, normal);
+        float3 viewDirection = normalize(-worldPos - rayOrigin);
+        float specular = pow(max(dot(reflection, viewDirection), 0), 200);
+
+        return (diffuse * float3(1,0,0)) + (specular * float3(1, 1, 1));
+    }
+
+    opacityMask = 1;
+    rayOrigin += rayStep;
+ }
+opacityMask = 0;
+return float3(0,0,0);
